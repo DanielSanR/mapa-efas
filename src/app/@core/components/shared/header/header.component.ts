e0f1e34e5760598dd3f510bacdf1fe95fdb5a0a7
Daniel Romero <danielromero320@gmail.com>
2021-01-20 08:48:22 -0300

    update backBtn

--- src/app/@core/components/shared/header/header.component.ts
+++ src/app/@core/components/shared/header/header.component.ts
@@ -1,26 +1,38 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, QueryList } from '@angular/core';
 import { Location } from '@angular/common'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
 
+export class HeaderComponent implements OnInit {
+  baseURL: boolean;
 
   @Output() public sidenavToggle = new EventEmitter();
-
-  constructor(private _location: Location) { }
+  
+  constructor(private _location: Location,private router: Router) { }
 
   ngOnInit(): void {
+    this.router.events.subscribe(event => {
+     if (this._location.path() !== '') {
+       this.baseURL = false;
+     } else {
+       this.baseURL = true;
+     }
+   });
+   
   }
 
   back():void {
+    console.log(this.baseURL );
     this._location.back();  
   }
 
   public onToggleSidenav = () => {
+  
     this.sidenavToggle.emit();
   }
 
